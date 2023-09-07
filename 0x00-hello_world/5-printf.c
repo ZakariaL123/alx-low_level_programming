29d39c9d1c3a71e1f8c6e4349d6710c2c5c62460
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-07 13:15:49 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * This C code block does the following:
+ * It uses printf to print the message "with proper grammar, but the outcome is a piece of art,"to the standard output.
+ * It then returns 0 to the operating system, indicating successful execution.
+ * Note: The message is split into two lines for readability and ends with a newline character (\n).
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
