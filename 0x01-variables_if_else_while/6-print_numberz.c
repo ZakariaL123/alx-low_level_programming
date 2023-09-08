047c3d34a9eb9925fbc550f9de69093e946c40ec
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-08 13:04:59 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: alphabet
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
