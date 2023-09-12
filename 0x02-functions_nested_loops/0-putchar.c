ba291b0e8f646b95900ce6ca3a2c5dd94765ecdd
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-12 16:19:54 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - test
+ *
+ * Description: print "_putchar"
+ *
+ * Return: return 0*/
+
+int main (void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readme file function c 
