4d19490574fdd463ab1bcd9228008796aa9071f2
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-12 19:52:40 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function to lower case checker
+ *
+ * @a: first value
+ *
+ * @b: second value
+ *
+ * Return: return 1 if c lower case else return 0
+*/
+
+int add(int a, int b)
+{
+	int result = a + b ;
+
+	return (result);
+}
