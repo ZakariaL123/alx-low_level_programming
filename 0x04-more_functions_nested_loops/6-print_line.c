4411ba4be684f16f6d439fe3297868bf3f8e7b45
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-14 15:28:31 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,12 @@ void print_line(int n)
 	{
 		_putchar('\n');
 	}
-	for (i = 0 ; i <= n ; i++)
+	else
 	{
-		_putchar('_' * i);
-	}
-	_putchar('\n');
+		for (i = 1 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}	
 }
