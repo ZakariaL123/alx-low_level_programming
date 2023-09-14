50cf00acf689f21d995724409cba447b566122c6
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-14 16:05:26 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to lower case checker
+ * @n: is the number of times the character _ should be printed
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1 ; i <= n ; i++)
+		{
+			_putchar('\\'):
+		}
+		_putchar('\n');
+	}
+}
