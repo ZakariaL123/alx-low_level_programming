4ce8596bde9276beab08be7853b6275c7cf58fc6
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-14 15:10:02 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to preint numbers
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int num;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			num = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				num = j % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
