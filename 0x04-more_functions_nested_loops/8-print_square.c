d877752f1d1ad8a09b8ba6d485baa684ee8e6d28
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-14 17:27:09 -0700

    10

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - function to lower case checker
+ * @size: number of '#'
+*/
+
+void print_square(int size)
+{
+        if (size <= 0)
+        {
+                _putchar('\n');
+        }
+        else
+        {
+                for (int i = 1 ; i < size ; i++)
+                {
+                        for (j = 1 ; j < size ; j++)
+                        {
+                                _putchar(35);
+                        }
+                        _putchar('\n');
+                }
+        }
+}
