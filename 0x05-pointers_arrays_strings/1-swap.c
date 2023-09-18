3601754bb6174ef8e8ad834d0054b15ab6e363dd
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-18 16:05:44 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap numbers
+ *
+ * @*a: first value
+ *
+ * @*b: second value
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	a = b;
+
+	b = temp;
+}
