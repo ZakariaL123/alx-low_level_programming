55168cef2cd14b0dfa41011a7908a0fe36f5c4d8
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-18 17:15:05 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to lower case checker
+ *
+ * @n: checks input of function
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void reset_to_98(int *n);
 
 #endif
