5fec1d72aeba7f41785f6292f5963b1c49321745
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-21 16:49:31 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - make two strings one
+ *
+ * @a: seconf string
+ *
+ * @n: numberof bytes
+ *
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+	for (int i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
