50240491bb368b45104eb0c21ad71e351c326182
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-21 15:52:59 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,28 +12,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
 
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		j++;
+		s1++;
+		s2++;
 	}
 
-	if (i < j)
-	{
-		return (-15);
-	}
-	else if (i > j)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (*s1 - *s2);
 }
