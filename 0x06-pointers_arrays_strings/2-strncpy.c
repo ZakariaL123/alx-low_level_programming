8a293e399409257954e6921507238f98e02916ce
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-21 15:20:30 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * _strncpy - make two strings one
+ *
+ * @dest: first string
+ *
+ * @src: seconf string
+ *
+ * @n: numberof bytes
+ *
+ * Return: return the value of string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
