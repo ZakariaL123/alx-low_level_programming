f080d5c3d3a2acaf8ea54fe69bd959ee867b00fe
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-25 13:04:59 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
-/***/
+/**
+ * _memset - the function
+ *
+ * @s: first value
+ *
+ * @b: second value
+ *
+ * @n: third value
+ *
+ * Return: return char
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
