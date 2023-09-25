e97be5924d60b7b243db1e086f49c32a190c1cb9
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-25 16:30:06 -0700

    3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (*s[i] != '\0')
 	{
-		if (*s == c)
+		if (*s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
+		i++;
 	}
 
-	return (null);
+	return ('\0');
 }
