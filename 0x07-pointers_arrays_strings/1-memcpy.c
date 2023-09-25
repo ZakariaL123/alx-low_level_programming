aa63ab266679aa18b23883e2d6936d196a68c3a2
ZakariaL123 <laouibizakaria63@gmail.com>
2023-09-25 13:53:59 -0700

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memcpy - the function
+ *
+ * @dest: first value
+ *
+ * @src: second value
+ *
+ * @n: third value
+ *
+ * Return: return char
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *original_dest = dest;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (original_dest);
+}
