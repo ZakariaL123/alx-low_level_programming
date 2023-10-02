7674360b55c0b931c70e4b0c7d74b09eaed67881
ZakariaL123 <laouibizakaria63@gmail.com>
2023-10-02 16:34:17 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argx: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *aargv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
