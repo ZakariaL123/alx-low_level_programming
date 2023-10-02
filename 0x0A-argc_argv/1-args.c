5112d44526487fc42fb4fc7069dd5431e8fe8e71
ZakariaL123 <laouibizakaria63@gmail.com>
2023-10-02 16:44:38 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
