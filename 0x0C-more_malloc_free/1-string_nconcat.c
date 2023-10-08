7a070735465925a692d3afcc1835363c1a6a7056
ZakariaL123 <laouibizakaria63@gmail.com>
2023-10-08 16:36:05 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s2[i];
+		str[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
