bd5253d3ec2fc5eed446676f4e60bccdefcacb43
ZakariaL123 <laouibizakaria63@gmail.com>
2023-10-11 13:29:09 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that print names
+ *
+ * @name: the name of user
+ *
+ * @f: function to pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
