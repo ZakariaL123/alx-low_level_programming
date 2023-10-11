468179139951571a2ce0d4dd43fb40345fd328a1
ZakariaL123 <laouibizakaria63@gmail.com>
2023-10-10 17:22:45 -0700

    2

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/***/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog != NULL)
+	{
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
+	}
+
+	return new_dog;
+}
